72c2dfbf69ac5f10ffd7d7ff87bb3c9824202f55
Kuprienko Anatolii <ipz222_kaa@student.ztu.edu.ua>
2023-04-17 15:50:11 +0300

    Added some styles and added annotation to CategoryModel

--- ToDoList/Models/CategoryModel.cs
+++ ToDoList/Models/CategoryModel.cs
@@ -1,8 +1,12 @@
-﻿namespace ToDoList.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
 {
     public class CategoryModel
     {
         public int id { get; set; }
+        [Display(Name="Title")]
+        [Required]
         public string name { get; set; }
     }
 }
